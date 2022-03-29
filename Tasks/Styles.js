2283c03c58a916288d13793d2d4680d8030fdb62
danielthomas74 <83643582+danielthomas74@users.noreply.github.com>
2022-03-29 13:34:35 +0200

    Use gulp-dart-sass instead of gulp-sass

--- Tasks/Styles.js
+++ Tasks/Styles.js
@@ -6,7 +6,7 @@
 ########################################################################################################################################################################################################
 */
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-dart-sass');
 var concat = require('gulp-concat');
 var cleanCSS = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
